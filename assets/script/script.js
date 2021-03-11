993c583346076a7d1251fc35e0256a2a829b43fe
minamahmood83 <minamahmood83@gmail.com>
2021-03-11 09:41:47 -0800

    working on result

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,5 +11,9 @@ btnWeather.onclick = function(event) {
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=39907d575f349b1b0380ef04160f3574`;
     fetch(url)
         .then((response) => response.json())
-        .then((json) => {});
+        .then((json) => {
+            name.innerHTML = json.name;
+            temp.innerHTML = json.main.temp;
+            weather.innerHTML = json.weather[0].main;
+        });
 };
