5d464316cd500764588bd157eaaf8074053d847f
minamahmood83 <minamahmood83@gmail.com>
2021-03-10 23:36:06 -0800

    add jsva script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,6 @@
-(function() {
-    $("#form-submit").submit(function(event) {
-        performSearch(event);
-    });
-});
-
-function performSearch(event) {
-    var request;
-    event.preventDefault();
-
-    request.ajax({
-        url: "api.openweathermap.org/data/2.5/forecast",
-        type: "GET",
-        data: {
-            q: $("#city").val(),
-            appid: "39907d575f349b1b0380ef04160f3574",
-            units: "metric",
-        },
-    });
-}
+const btnWeather = document.getElementById("btnWeather");
+const txtCity = document.getElementById("txtCity");
+const resultOut = document.getElementById("result");
+const name = document.getElementById("name");
+const weather = document.getElementById("weather");
+const temp = document.getElementById("temp");
