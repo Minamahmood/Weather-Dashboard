bb84e89caa4438797b0073b9d5f582e5b3eef61e
minamahmood83 <minamahmood83@gmail.com>
2021-03-11 00:01:00 -0800

    added url kay

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,12 @@ const resultOut = document.getElementById("result");
 const name = document.getElementById("name");
 const weather = document.getElementById("weather");
 const temp = document.getElementById("temp");
+
+btnWeather.onclick = function(event) {
+    event.preventDefault();
+    const city = txtCity.value;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=39907d575f349b1b0380ef04160f3574`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((json) => {});
+};
